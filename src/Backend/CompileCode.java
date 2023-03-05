07510744bac9613dd357e866ed8e9d4308ec9637
Nico / Varrix <nicosalm04@gmail.com>
2023-03-04 20:36:32 -0600

    printing backend output to terminal

--- src/Backend/CompileCode.java
+++ src/Backend/CompileCode.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.*;
 
-
 public class CompileCode {
 
     private boolean status; // status of the compilation
@@ -13,28 +12,21 @@ public class CompileCode {
 
     public CompileCode(String name) {
         this.name = name;
-        status = true;
     }
 
     public static void main(String[] args) {
         CompileCode c = new CompileCode("Test");
-        c.start();    
+        c.start();
     }
 
     private void start() {
         try {
             Class<?> c = Class.forName("src.Backend." + name);
             Method m = c.getDeclaredMethod("main", String[].class);
-            System.out.println(status);
             m.invoke(null, (Object) new String[] {});
-            
         } catch (Exception e) {
-            status = false;
-            System.out.println(status);
-            for (StackTraceElement element : e.getStackTrace()) {
-                System.out.println(element);
-            }
+            System.out.println(false + "\n" + e.getCause());
         }
-    }   
+    }
 }
  
