29e6a4007b5893e248d462e2709d855e7947a01a
Phong Do <pdo2@wisc.edu>
2023-03-04 18:55:51 -0600

    pre-integration, waiting for partners

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const axios = require('axios');
 const imageProcessing = require('./src/Backend/imageProcessing');
 const express = require("express");
 const app = express();
+const cors = require('cors')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 const PORT = 8080;
   
-app.post("/post", (req, res) => {
-  console.log("Connected to React");
-  res.redirect("/");
+app.get("/", cors(), (req, res) => {
+  console.log("collected data from frontend");
+
+});
+
+app.post("/", async (req, res) => {
+  console.log("collected data from frontend");
+  const { data } = req.body;
 });
   
   
